2a93d7cf3ad7acaa3b2faaadac4608a6426d5563
hedgemaze <hedgemazeexpo@gmail.com>
2026-06-05 11:18:08 -0400

    Revert "Update svelte.config.js"
    This reverts commit d7b130600cc4bf7113aac406834de0e51b113130.

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,23 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-node';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { importAssets} from "svelte-preprocess-import-assets";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: [
+		importAssets(),
+		vitePreprocess({ script: true }),
+	],
+
 	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.https://hedgemaze.github.io/HungerGamesSimulator
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adaptersXS for more information about adapters.
+		adapter: adapter(),
+		files: {
+			assets: 'static',
 		}
 	}
 };
