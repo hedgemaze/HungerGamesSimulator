d7b130600cc4bf7113aac406834de0e51b113130
hedgemaze <hedgemazeexpo@gmail.com>
2026-06-05 11:02:44 -0400

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,23 +1,13 @@
-import adapter from '@sveltejs/adapter-node';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { importAssets} from "svelte-preprocess-import-assets";
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: [
-		importAssets(),
-		vitePreprocess({ script: true }),
-	],
-
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adaptersXS for more information about adapters.
-		adapter: adapter(),
-		files: {
-			assets: 'static',
+		adapter: adapter({
+			fallback: '404.html'
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.https://hedgemaze.github.io/HungerGamesSimulator
 		}
 	}
 };
